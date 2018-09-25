5e71faa58060047e6630c8c4991ae7e576069f36
Marco Aneloa <marco.aneloa@msp.gob.ec>
2018-09-25 13:53:19 -0500

    funcion con flecha

--- app.js
+++ app.js
@@ -31,10 +31,52 @@ let spider= {
     edad:12
 }*/
 //Template Literales
-var nombre = "Juan";
-var apellido = "Perez";
-var edad = 45;
+/*let nombre:string ="Juan";
+let apellido:string="Perez";
+let edad:number=45;
+
 //let texto:string="Hola "+nombre+" "+apellido+" ("+edad+")";
-var texto = "Hola, " + nombre + " " + apellido + " (" + edad + ")";
-var texto2 = "" + (1 + 2);
-console.log(texto2);
+let texto=`Hola, ${nombre} ${apellido} (${edad})`;
+
+let texto2:string=`${1+2}` //${aqui puede estar codigo de javascript}
+
+console.log(texto2);*/
+//FUNCIONES
+//parametro por defecto: variable?:string
+/*function activar(quien:string,objeto:string="batiseñal",momento?:string){
+    let mensaje:string;
+
+    if(momento){
+        mensaje=`${quien} activó la ${objeto} en la ${momento}`;
+    }else{
+        mensaje=`${quien} activó la ${objeto}`;
+    }
+
+    console.log(mensaje);
+}
+
+activar("Gordon,");*/
+//funciones flechas
+/*let funcion=function(a:number,b:number){
+    return a+b;
+}
+
+let funcinf=(a:number,b:number) => a+b;
+*/
+//let nombre="perdro";
+let hulk = {
+    nombre: "hulk",
+    smash() {
+        setTimeout(function () {
+            console.log(hulk.nombre + " smash!!!");
+        }, 1500);
+    }
+};
+let hulk2 = {
+    nombre: "hulk",
+    smash() {
+        setTimeout(() => console.log(hulk.nombre + " smash!!!"), 1500);
+    }
+};
+hulk.smash();
+hulk2.smash();
--- app.ts
+++ app.ts
@@ -33,7 +33,7 @@ let spider= {
 }*/
 
 //Template Literales
-let nombre:string ="Juan";
+/*let nombre:string ="Juan";
 let apellido:string="Perez";
 let edad:number=45;
 
@@ -42,4 +42,49 @@ let texto=`Hola, ${nombre} ${apellido} (${edad})`;
 
 let texto2:string=`${1+2}` //${aqui puede estar codigo de javascript}
 
-console.log(texto2);
+console.log(texto2);*/
+
+//FUNCIONES
+//parametro por defecto: variable?:string
+
+/*function activar(quien:string,objeto:string="batiseñal",momento?:string){
+    let mensaje:string;
+
+    if(momento){
+        mensaje=`${quien} activó la ${objeto} en la ${momento}`;        
+    }else{
+        mensaje=`${quien} activó la ${objeto}`;
+    }
+
+    console.log(mensaje);
+}
+
+activar("Gordon,");*/
+
+//funciones flechas
+/*let funcion=function(a:number,b:number){
+    return a+b;
+}
+
+let funcinf=(a:number,b:number) => a+b;
+*/
+
+//let nombre="perdro";
+
+let hulk={
+    nombre:"hulk",
+    smash(){
+        setTimeout(function(){
+            console.log(hulk.nombre + " smash!!!");
+        },1500);
+    }
+}
+let hulk2={
+    nombre:"hulk",
+    smash(){
+        setTimeout(()=> console.log(hulk.nombre + " smash!!!"),1500);
+    }
+}
+
+hulk.smash();
+hulk2.smash();
